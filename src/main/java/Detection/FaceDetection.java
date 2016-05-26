acf336d1c0f9a86a0aeabc53e47368673e192402
Viker94 <mfilipek94@gmail.com>
2016-05-26 14:16:03 +0200

    OpenCV wykrywanie twarzy TESTv1

--- src/main/java/Detection/FaceDetection.java
+++ src/main/java/Detection/FaceDetection.java
@@ -10,13 +10,21 @@ import org.opencv.objdetect.CascadeClassifier;
 public class FaceDetection {
     public void faceDetection() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        CascadeClassifier faceDetector = new CascadeClassifier(FaceDetection.class.getResource("haarcascade_frontalface_alt.xml").getPath());
-        Mat image = Highgui.imread(FaceDetection.class.getResource("combine_images.jpg").getPath());
+        CascadeClassifier faceDetector = new CascadeClassifier();
+        faceDetector.load("C:/OpenCV/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml");
+        CascadeClassifier eyeDetector = new CascadeClassifier();
+        eyeDetector.load("C:/OpenCV/opencv/sources/data/haarcascades/haarcascade_eye.xml");
+        Mat image = Highgui.imread("combine_images.jpg");
         MatOfRect faceDetections = new MatOfRect();
         faceDetector.detectMultiScale(image, faceDetections);
         System.out.println("Wykryto " + faceDetections.toArray().length + " twarzy");
+        MatOfRect eyes = new MatOfRect();
+        eyeDetector.detectMultiScale(image, eyes);
         for(Rect rect : faceDetections.toArray()){
             Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+        }
+        for (Rect rect : eyes.toArray()){
+            Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255));
         }
         System.out.println("Zapis do pliku");
         Highgui.imwrite("output.jpg", image);
